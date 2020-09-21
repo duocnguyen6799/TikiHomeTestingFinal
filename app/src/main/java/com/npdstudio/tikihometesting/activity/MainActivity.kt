ae288fa7a19bb33ec0fa2a3af6522a9c8675ad30
Nguyễn Phục Dược <41044684+duocnguyen6799@users.noreply.github.com>
2020-09-21 23:12:05 +0700

    add library Retrofit and gson converter

--- app/src/main/java/com/npdstudio/tikihometesting/activity/MainActivity.kt
+++ app/src/main/java/com/npdstudio/tikihometesting/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.npdstudio.tikihometesting
+package com.npdstudio.tikihometesting.activity
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.npdstudio.tikihometesting.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,8 +21,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard,
-                R.id.navigation_search, R.id.navigation_notifications,
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_search,
+                R.id.navigation_notifications,
                 R.id.navigation_profile
             )
         )
